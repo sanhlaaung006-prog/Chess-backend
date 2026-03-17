13c9e5bf2315baeb036ac87c1110241f1d5f73d0
sanhlaaung006-prog <sanhlaaung006@gmail.com>
2026-03-17 12:42:55 +0630

    Refactor server.js to include package.json structure
    Removed existing server code and added package.json structure.

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-const express = require('express');
-const http = require('http');
-const { Chess } = require('chess.js') || { Chess: class { constructor() { this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'; move() { return true; } } };
-
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server, { cors: { origin: "*" } });
-const games = {};
-
-server.listen(process.env.PORT || 3000);
-
-io.on('connection', socket => {
-  socket.on('join', room => socket.join(room));
-  socket.on('move', ({room, move}) => {
-    const game = games[room] || (games[room] = new Chess());
-    if (game.move(move)) io.to(room).emit('fen', game.fen());
-  });
-});
+{
+  "name": "chess-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {"start": "node server.js"},
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
