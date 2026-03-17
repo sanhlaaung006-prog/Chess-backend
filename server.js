1513032691e621efc9e3c5a3e7ebfe6a63fedb87
sanhlaaung006-prog <sanhlaaung006@gmail.com>
2026-03-17 13:34:31 +0630

    Enhance join and move events for game rooms
    Refactor socket event handling for game rooms and player management.

--- server.js
+++ server.js
@@ -7,9 +7,23 @@ const io = new Server(server, { cors: { origin: "*" } });
 const games = {};
 
 io.on('connection', socket => {
-  socket.on('join', room => socket.join(room));
-  socket.on('move', ({room, move}) => {
-    const game = games[room] || (games[room] = new Chess());
-    if (game.move(move)) io.to(room).emit('fen', game.fen());
+  socket.on('join', ({room, name = 'Anonymous'} = {}) => {
+    socket.join(room);
+    if (!games[room]) {
+      games[room] = { 
+        chess: new Chess(), 
+        players: {}, 
+        turn: 'w' 
+      };
+    }
+    games[room].players[socket.id] = name;
+    io.to(room).emit('players', Object.values(games[room].players));
+  });
+  
+  socket.on('move', ({room, from, to, promotion = 'q'}) => {
+    const game = games[room];
+    if (game && game.chess.move({from, to, promotion})) {
+      io.to(room).emit('fen', game.chess.fen());
+    }
   });
 });
